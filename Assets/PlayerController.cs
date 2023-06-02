b1c51d52915b9912b9cb38cc22bdf40205b2d0a9
iwannatto <iwannatto@gmail.com>
2023-06-02 17:52:31 +0900

    Add jump motion

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     float jumpForce = 680.0f;
     float walkForce = 30.0f;
     float maxWalkSpeed = 2.0f;
+    float threshold = 0.2f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,14 +23,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
+        if (Input.GetMouseButtonDown(0) && this.rigid2D.velocity.y == 0)
         {
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * jumpForce);
         }
 
         int key = 0;
-        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+        if (Input.acceleration.x > this.threshold) key = 1;
+        if (Input.acceleration.x < -this.threshold) key = -1;
 
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
 
@@ -42,7 +44,14 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(key, 1, 1);
         }
 
-        this.animator.speed = speedx / 2.0f;
+        if (this.rigid2D.velocity.y == 0)
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+        }
 
         if (transform.position.y < -10)
         {
