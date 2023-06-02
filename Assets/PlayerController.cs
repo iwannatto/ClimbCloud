f13758ab5bfb32e70c23451a6c4c7b5341339482
iwannatto <iwannatto@gmail.com>
2023-06-02 16:48:36 +0900

    Fix infinity jump bug and Reset when player is out

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
         {
             this.rigid2D.AddForce(transform.up * jumpForce);
         }
@@ -43,6 +43,11 @@ public class PlayerController : MonoBehaviour
         }
 
         this.animator.speed = speedx / 2.0f;
+
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
